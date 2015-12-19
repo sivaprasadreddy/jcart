be64baf8d182fae1699e1274791aafecbbab51f8
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2015-12-19 08:47:27 +0530

    Implementing Manage Roles functionality.

--- jcart-core/src/main/java/com/sivalabs/jcart/security/SecurityService.java
+++ jcart-core/src/main/java/com/sivalabs/jcart/security/SecurityService.java
@@ -3,6 +3,7 @@
  */
 package com.sivalabs.jcart.security;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,6 +14,7 @@ import org.springframework.util.StringUtils;
 
 import com.sivalabs.jcart.JCartException;
 import com.sivalabs.jcart.entities.Permission;
+import com.sivalabs.jcart.entities.Role;
 import com.sivalabs.jcart.entities.User;
 
 /**
@@ -25,6 +27,7 @@ public class SecurityService
 {
 	@Autowired UserRepository userRepository;
 	@Autowired PermissionRepository permissionRepository;
+	@Autowired RoleRepository roleRepository;
 	
 	public User findUserByEmail(String email)
 	{
@@ -74,4 +77,58 @@ public class SecurityService
 		return permissionRepository.findAll();
 	}
 
+	public List<Role> getAllRoles() {
+		return roleRepository.findAll();
+	}
+
+	public Role getRoleByName(String roleName)
+	{
+		return roleRepository.findByName(roleName);
+	}
+	
+	public Role createRole(Role role)
+	{
+		Role roleByName = getRoleByName(role.getName());
+		if(roleByName != null){
+			throw new JCartException("Role "+role.getName()+" already exist");
+		}
+		List<Permission> persistedPermissions = new ArrayList<>();
+		List<Permission> permissions = role.getPermissions();
+		if(permissions != null){
+			for (Permission permission : permissions) {
+				if(permission.getId() != null)
+				{
+					persistedPermissions.add(permissionRepository.findOne(permission.getId()));
+				}
+			}
+		}
+		
+		role.setPermissions(persistedPermissions);
+		return roleRepository.save(role);
+	}
+	
+	public Role updateRole(Role role)
+	{
+		Role persistedRole = getRoleById(role.getId());
+		if(persistedRole == null){
+			throw new JCartException("Role "+role.getId()+" doesn't exist");
+		}
+		persistedRole.setDescription(role.getDescription());
+		List<Permission> updatedPermissions = new ArrayList<>();
+		List<Permission> permissions = role.getPermissions();
+		if(permissions != null){
+			for (Permission permission : permissions) {
+				if(permission.getId() != null)
+				{
+					updatedPermissions.add(permissionRepository.findOne(permission.getId()));
+				}
+			}
+		}
+		persistedRole.setPermissions(updatedPermissions);
+		return roleRepository.save(persistedRole);
+	}
+	
+	public Role getRoleById(Integer id) {
+		return roleRepository.findOne(id);
+	}
 }
